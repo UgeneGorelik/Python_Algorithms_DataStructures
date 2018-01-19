6e160156c5eff7df5ddc8464ee25004d4532ee92
Ivgeny Gorelik <34338008+UgeneGorelik@users.noreply.github.com>
2018-01-19 13:37:37 +0200

    Update Kadane.py

--- Kadane.py
+++ Kadane.py
@@ -1,10 +1,9 @@
 def solution(A):
-    max_sum=0
-    cur_sum=0
-    for x in range(0,len(A)-1):
-        cur_sum=max(cur_sum+A[x],cur_sum)
-        max_sum=max(max_sum,cur_sum)
-    print(max_sum)
+ max_ending = max_slice = 0
+ for a in A:
+    max_ending = max(0, max_ending + a)
+    max_slice = max(max_slice, max_ending)
+ return max_slice
 
 
 solution([-2,3,2,-1])
